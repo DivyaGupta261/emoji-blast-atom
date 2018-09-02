41239ef1d2db2a4b3e2699b67751efd6a08ab369
Divya Gupta <divya261261@yahoo.com>
2018-09-02 14:36:27 +0530

    Initial commit

---
+++ spec/emoji-blast-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EmojiBlastView from '../lib/emoji-blast-view';
+
+describe('EmojiBlastView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
